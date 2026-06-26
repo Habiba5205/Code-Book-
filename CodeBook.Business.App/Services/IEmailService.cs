2e07cb40843e9b8e4239b6bb27b506d443865822
Habiba5205 <es.habiba.mamoun2023@alexu.edu.eg>
2026-06-26 18:11:32 +0300

    Edits in Interfaces

--- CodeBook.Business.App/Services/IEmailService.cs
+++ CodeBook.Business.App/Services/IEmailService.cs
@@ -3,7 +3,7 @@ namespace CodeBook.Business.App.Services
 {
     public interface IEmailService
     {
-        Task SendVerificationEmail(string email, string cerificationLink);
-        Task SendPasswordResetEmail(string email, string resetLink);
+        void SendVerificationEmail(string email, string cerificationLink);
+        void SendPasswordResetEmail(string email, string resetLink);
     }
 }
--- CodeBook.Business.App/Services/IModerationService.cs
+++ CodeBook.Business.App/Services/IModerationService.cs
@@ -4,6 +4,6 @@ namespace CodeBook.Business.App.Services
 {
 	public interface IModerationService
     {
-		Task RemovePostAsync(int removerid, int postId, int reportId, string reason);
+		void RemovePost(int removerid, int postId, int reportId, string reason);
 	}
 }
