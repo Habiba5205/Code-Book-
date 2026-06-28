619e9c5d82250f9b1e5a030ba88e7ac3d993b087
Habiba5205 <es.habiba.mamoun2023@alexu.edu.eg>
2026-06-29 00:32:42 +0300

    Missing File

---
+++ CodeBook.Business.App/DTOs/AddCommentRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBook.Business.App.DTOs
+{
+    public class AddCommentRequest
+    {
+        public int AuthorId { get; set; }
+        public string Body { get; set; }
+        public int? SelfCommentId { get; set; }
+    }
+}
