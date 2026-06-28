83a3b1b9d9a5779509e44e76c680235c52b88547
rouaa18 <es.rouaa.bahaaeldin2023@alexu.edu.eg>
2026-06-28 16:04:11 +0300

    Add API layer

---
+++ CodeBook.Data.App/CodeBook.API.App/Controllers/AuthController.cs
@@ -0,0 +1,10 @@
+﻿using CodeBook.Business.App.DTOs;
+using CodeBook.Business.App.Services;
+using Microsoft.AspNetCore.Mvc;
+namespace CodeBook.API.App.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+
+    }
+}
