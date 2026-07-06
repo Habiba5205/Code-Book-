ba59e6b641c076c23409e1c619fb2078daf45d52
Farida-Yousry <es.farida.yousry2023@alexu.edu.eg>
2026-07-07 00:21:37 +0300

    added Theme

--- CodeBook.View.App/JS/theme.js
+++ CodeBook.View.App/JS/theme.js
@@ -1,20 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
 
-    function toggleTheme(){
-        const checkbox = document.getElementById('themeToggle');
-        document.body.classList.toggle('light-mode',checkbox.checked);
-        document.cookie = `theme=${checkbox.checked ? 'light' : 'dark'}; path=/; max-age=31536000`;
+    const toggle = document.getElementById("themeToggle");
+    const match = document.cookie.match(/theme=(light|dark)/);
+
+    if (match && match[1] === "light") {
+        document.body.classList.add("light-theme");
+        if (toggle) {
+            toggle.checked = true;
+        }
     }
-        (function() {
-            const match = document.cookie.match(/theme=(light|dark)/);
-            if(match && match[1] === 'light'){
-                document.body.classList.add('light-mode');
-                window.addEventListener('DOMContentLoaded',() =>{
-                    document.getElementById('themeToggle');
-                    const toggle = document.getElementById("themeToggle");
-       if (toggle) {
-        toggle.checked = true;
-      }
-                });
+
+    if (toggle) {
+        toggle.addEventListener("change", () => {
+
+            if (toggle.checked) {
+                document.body.classList.add("light-theme");
+                document.cookie = "theme=light; path=/; max-age=31536000";
+            } else {
+                document.body.classList.remove("light-theme");
+                document.cookie = "theme=dark; path=/; max-age=31536000";
             }
-        })();
-    
+
+        });
+    }
+
+});
