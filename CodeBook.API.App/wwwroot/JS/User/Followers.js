31716144d7fadcc87ec71d64b5c4e68218b7ad5e
Farida-Yousry <es.farida.yousry2023@alexu.edu.eg>
2026-07-10 00:22:08 +0300

    edits

--- CodeBook.API.App/wwwroot/JS/User/Followers.js
+++ CodeBook.API.App/wwwroot/JS/User/Followers.js
@@ -20,7 +20,7 @@ async function loadFollowers(followersContainer) {
             const followerCard = document.createElement('div');
             followerCard.className = 'follower-card';
             followerCard.innerHTML = `
-                <a href="OtherUserProfile.html?userId=${user.id}" class="card p-4 text-decoration-none text-dark shadow-sm hover-card">
+                <a href="OtherUserProfile.html?userId=${user.id}" class="card p-4 text-decoration-none shadow-sm hover-card">
                 <div class="row align-items-center">
                 <div class="col-md-3 text-center">
                 <img src="${user.avatarUrl ? user.avatarUrl : 'https://i.pinimg.com/originals/60/b6/6f/60b66f7e6337e0de45cea924a3946dbd.png'}"
@@ -29,7 +29,7 @@ async function loadFollowers(followersContainer) {
                 </div>
                 <div class="col-md-9">
                 <h2 class="follower-name mb-1">${user.userName}</h2>
-                <p class="follower-bio-50">${user.bio || 'No bio yet'}</p>
+                <p class="follower-bio">${user.bio || 'No bio yet'}</p>
                 </div>
                 </div>
                 </a>`;
